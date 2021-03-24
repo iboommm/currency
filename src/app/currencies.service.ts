8ec1a30def7e3d6c1fd20a7cb101cca144ec26e2
iboommm <iboommm@live.com>
2021-03-24 10:45:27 +0700

    Update services

--- src/app/currencies.service.ts
+++ src/app/currencies.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import axios from 'axios';
+import { HttpClient } from '@angular/common/http';
+import { CurrencyModel } from './models/CurrencyModel';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CurrenciesService {
-  constructor() {}
+  constructor(public http: HttpClient) {}
 
   getRates(base: string): Promise<object> {
     return new Promise((resolve, reject) => {
@@ -15,6 +17,25 @@ export class CurrenciesService {
           resolve(x.data);
         })
         .catch((e) => reject(e));
+    });
+  }
+
+  async getAllCurrencies(): Promise<CurrencyModel[]> {
+    return new Promise(async (resolve, reject) => {
+      this.http.get('assets/data/currencies.json').subscribe((response) => {
+        const tmp: CurrencyModel[] = response['data'].map(
+          (x): CurrencyModel => {
+            x.value = '1000';
+            x.editable = false;
+            x.favorite = false;
+            x.image = `../../assets/icon/national/${x.key
+              .slice(0, -1)
+              .toLocaleLowerCase()}.png`;
+            return x;
+          }
+        );
+        resolve(tmp);
+      });
     });
   }
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { AlertController, ActionSheetController } from '@ionic/angular';
 import { CurrenciesService } from '../currencies.service';
@@ -16,59 +15,12 @@ export class Tab1Page {
   searchToggle: boolean = false;
   searchTxt: string = '';
 
-  list: CurrencyModel[] = [
-    {
-      key: 'THB',
-      name: 'Thai baht',
-      editable: false,
-      favorite: false,
-      value: '1000',
-    },
-  ];
+  list: CurrencyModel[] = [];
 
-  rate = {
-    rates: {
-      CAD: 1.2496226732,
-      HKD: 7.765721952,
-      ISK: 126.7818212309,
-      PHP: 48.5015931578,
-      DKK: 6.2351165521,
-      HUF: 307.6471574711,
-      CZK: 21.8639946336,
-      GBP: 0.723067248,
-      RON: 4.0970987758,
-      SEK: 8.5175247359,
-      IDR: 14402.4987422438,
-      INR: 72.3541002851,
-      BRL: 5.5271675331,
-      RUB: 74.5439376153,
-      HRK: 6.3508301191,
-      JPY: 108.8126781821,
-      THB: 30.8703672648,
-      CHF: 0.924283079,
-      EUR: 0.8385041087,
-      MYR: 4.1140365588,
-      BGN: 1.6399463357,
-      TRY: 7.8900721114,
-      CNY: 6.5052825759,
-      NOK: 8.501844709,
-      NZD: 1.3933422774,
-      ZAR: 14.7289116217,
-      USD: 1,
-      MXN: 20.6554586617,
-      SGD: 1.3409357706,
-      AUD: 1.2912963274,
-      ILS: 3.3040415898,
-      KRW: 1127.6035552574,
-      PLN: 3.8585443569,
-    },
-    base: 'USD',
-    date: '2021-03-22',
-  };
+  rate: Object;
 
   constructor(
     public alertController: AlertController,
-    public http: HttpClient,
     public actionSheetController: ActionSheetController,
     public currenciesServie: CurrenciesService
   ) {
@@ -78,7 +30,7 @@ export class Tab1Page {
   async main() {
     const list = localStorage.getItem('list');
     if (!list) {
-      this.list = await this.getAllCurrencies();
+      this.list = await this.currenciesServie.getAllCurrencies();
       localStorage.setItem('list', JSON.stringify(this.list));
     } else {
       this.list = JSON.parse(localStorage.getItem('list'));
@@ -93,25 +45,6 @@ export class Tab1Page {
       this.selected = JSON.parse(localStorage.getItem('selected'));
     }
     await this.getNewRates();
-  }
-
-  async getAllCurrencies(): Promise<CurrencyModel[]> {
-    return new Promise(async (resolve, reject) => {
-      this.http.get('assets/data/currencies.json').subscribe((response) => {
-        const tmp: CurrencyModel[] = response['data'].map(
-          (x): CurrencyModel => {
-            x.value = '1000';
-            x.editable = false;
-            x.favorite = false;
-            x.image = `../../assets/icon/national/${x.key
-              .slice(0, -1)
-              .toLocaleLowerCase()}.png`;
-            return x;
-          }
-        );
-        resolve(tmp);
-      });
-    });
   }
 
   async doRefresh(event) {
